23d0aaa4a6b58e5b90dd7af6f34a0f36122df020
Lillu70 <122602146+Lillu70@users.noreply.github.com>
2023-11-03 11:53:50 +0200

    Update Score.cpp

--- src/Game/Score.cpp
+++ src/Game/Score.cpp
@@ -11,31 +11,31 @@ static u32 s_highscores[s_max_highscore_count] = {0};
 
 struct Score_Package
 {
-    u32 score = 0;
-    u32 ranking = 0;
+	u32 score = 0;
+	u32 ranking = 0;
 };
 static Score_Package s_last_earned_score = {0};
 
 
 static u32 add_new_score(u32 score)
 {
-    u32 ranking = 0;
-    
-    for(u32 i = 0; i < s_max_highscore_count; ++i)
-    {
-        if(score > s_highscores[i])
-        {
-            // Shift down the other scores, exept the last one.
-            for(u32 y = s_max_highscore_count - 1; y > i; --y)
-                s_highscores[y] = s_highscores[y - 1];
-            
-            s_highscores[i] = score;
-            ranking = i + 1;
-            break;
-        }
-    }
-    
-    s_last_earned_score = { score, ranking };
-    
-    return ranking;
+	u32 ranking = 0;
+	
+	for(u32 i = 0; i < s_max_highscore_count; ++i)
+	{
+		if(score > s_highscores[i])
+		{
+			// Shift down the other scores, exept the last one.
+			for(u32 y = s_max_highscore_count - 1; y > i; --y)
+				s_highscores[y] = s_highscores[y - 1];
+			
+			s_highscores[i] = score;
+			ranking = i + 1;
+			break;
+		}
+	}
+	
+	s_last_earned_score = { score, ranking };
+	
+	return ranking;
 }
