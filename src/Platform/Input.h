b0daf29ca55fc524ad3181e54fb49bbec87a106e
Lillu70 <122602146+Lillu70@users.noreply.github.com>
2023-11-03 11:42:10 +0200

    Update Input.h

--- src/Platform/Input.h
+++ src/Platform/Input.h
@@ -8,243 +8,243 @@
 
 enum class Key_Code
 {
-    NONE = -1,
-    NUM_0 = 0,
-    NUM_1,
-    NUM_2,
-    NUM_3,
-    NUM_4,
-    NUM_5,
-    NUM_6,
-    NUM_7,
-    NUM_8,
-    NUM_9,
-    NP_NUM_0,
-    NP_NUM_1,
-    NP_NUM_2,
-    NP_NUM_3,
-    NP_NUM_4,
-    NP_NUM_5,
-    NP_NUM_6,
-    NP_NUM_7,
-    NP_NUM_8,
-    NP_NUM_9,
-    NP_DIVIDE,
-    NP_MULTIPLY,
-    NP_ADD,
-    NP_SUBTRACT,
-    NP_DECIMAL,
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-    H,
-    I,
-    J,
-    K,
-    L,
-    M,
-    N,
-    O,
-    P,
-    Q,
-    R,
-    S,
-    T,
-    U,
-    V,
-    W,
-    X,
-    Y,
-    Z,
-    F1,
-    F2,
-    F3,
-    F4,
-    F5,
-    F6,
-    F7,
-    F8,
-    F9,
-    F10,
-    F11,
-    F12,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN,
-    ESC,
-    LCTRL,
-    LSHIFT,
-    LALT,
-    LSYS,
-    MOUSE_LEFT,
-    RCTRL,
-    RSHIFT,
-    RALT,
-    RSYS,
-    MOUSE_RIGHT,
-    SPACE,
-    PAGEUP,
-    PAGEDOWN,
-    END,
-    HOME,
-    INSERT,
-    DEL,
-    BACK,
-    ENTER,
-    COUNT
+	NONE = -1,
+	NUM_0 = 0,
+	NUM_1,
+	NUM_2,
+	NUM_3,
+	NUM_4,
+	NUM_5,
+	NUM_6,
+	NUM_7,
+	NUM_8,
+	NUM_9,
+	NP_NUM_0,
+	NP_NUM_1,
+	NP_NUM_2,
+	NP_NUM_3,
+	NP_NUM_4,
+	NP_NUM_5,
+	NP_NUM_6,
+	NP_NUM_7,
+	NP_NUM_8,
+	NP_NUM_9,
+	NP_DIVIDE,
+	NP_MULTIPLY,
+	NP_ADD,
+	NP_SUBTRACT,
+	NP_DECIMAL,
+	A,
+	B,
+	C,
+	D,
+	E,
+	F,
+	G,
+	H,
+	I,
+	J,
+	K,
+	L,
+	M,
+	N,
+	O,
+	P,
+	Q,
+	R,
+	S,
+	T,
+	U,
+	V,
+	W,
+	X,
+	Y,
+	Z,
+	F1,
+	F2,
+	F3,
+	F4,
+	F5,
+	F6,
+	F7,
+	F8,
+	F9,
+	F10,
+	F11,
+	F12,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	ESC,
+	LCTRL,
+	LSHIFT,
+	LALT,
+	LSYS,
+	MOUSE_LEFT,
+	RCTRL,
+	RSHIFT,
+	RALT,
+	RSYS,
+	MOUSE_RIGHT,
+	SPACE,
+	PAGEUP,
+	PAGEDOWN,
+	END,
+	HOME,
+	INSERT,
+	DEL,
+	BACK,
+	ENTER,
+	COUNT
 };
 
 static const char* s_key_names[] = {
-    "NUM 0",
-    "NUM 1",
-    "NUM 2",
-    "NUM 3",
-    "NUM 4",
-    "NUM 5",
-    "NUM 6",
-    "NUM 7",
-    "NUM 8",
-    "NUM 9",
-    "NP NUM_0",
-    "NP NUM_1",
-    "NP NUM_2",
-    "NP NUM_3",
-    "NP NUM_4",
-    "NP NUM_5",
-    "NP NUM_6",
-    "NP NUM_7",
-    "NP NUM_8",
-    "NP NUM_9",
-    "NP DIVIDE",
-    "NP MULTIPLY",
-    "NP ADD",
-    "NP SUBTRACT",
-    "NP DECIMAL",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "F1",
-    "F2",
-    "F3",
-    "F4",
-    "F5",
-    "F6",
-    "F7",
-    "F8",
-    "F9",
-    "F10",
-    "F11",
-    "F12",
-    "LEFT",
-    "RIGHT",
-    "UP",
-    "DOWN",
-    "ESC",
-    "LEFT CTRL",
-    "LEFT SHIFT",
-    "LEFT ALT",
-    "LEFT SYS",
-    "LEFT MOUSE",
-    "RIGHT CTRL",
-    "RIGHT SHIFT",
-    "RIGHT ALT",
-    "RIGHT SYS",
-    "RIGHT MOUSE",
-    "SPACE",
-    "PAGE UP",
-    "PAGE DOWN",
-    "END",
-    "HOME",
-    "INSERT",
-    "DEL",
-    "BACK",
-    "ENTER",
+	"NUM 0",
+	"NUM 1",
+	"NUM 2",
+	"NUM 3",
+	"NUM 4",
+	"NUM 5",
+	"NUM 6",
+	"NUM 7",
+	"NUM 8",
+	"NUM 9",
+	"NP NUM_0",
+	"NP NUM_1",
+	"NP NUM_2",
+	"NP NUM_3",
+	"NP NUM_4",
+	"NP NUM_5",
+	"NP NUM_6",
+	"NP NUM_7",
+	"NP NUM_8",
+	"NP NUM_9",
+	"NP DIVIDE",
+	"NP MULTIPLY",
+	"NP ADD",
+	"NP SUBTRACT",
+	"NP DECIMAL",
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"F1",
+	"F2",
+	"F3",
+	"F4",
+	"F5",
+	"F6",
+	"F7",
+	"F8",
+	"F9",
+	"F10",
+	"F11",
+	"F12",
+	"LEFT",
+	"RIGHT",
+	"UP",
+	"DOWN",
+	"ESC",
+	"LEFT CTRL",
+	"LEFT SHIFT",
+	"LEFT ALT",
+	"LEFT SYS",
+	"LEFT MOUSE",
+	"RIGHT CTRL",
+	"RIGHT SHIFT",
+	"RIGHT ALT",
+	"RIGHT SYS",
+	"RIGHT MOUSE",
+	"SPACE",
+	"PAGE UP",
+	"PAGE DOWN",
+	"END",
+	"HOME",
+	"INSERT",
+	"DEL",
+	"BACK",
+	"ENTER",
 };
 
 enum class Button
 {
-    NONE = -1,
-    DPAD_UP = 0, 
-    DPAD_DOWN,
-    DPAD_LEFT,
-    DPAD_RIGHT,
-    START,
-    BACK,
-    L_THUMB,
-    R_THUMB,
-    L_SHLD,
-    R_SHLD,
-    BUT_A,
-    BUT_B,
-    BUT_X,
-    BUT_Y, 
-    BUTTON_COUNT
+	NONE = -1,
+	DPAD_UP = 0, 
+	DPAD_DOWN,
+	DPAD_LEFT,
+	DPAD_RIGHT,
+	START,
+	BACK,
+	L_THUMB,
+	R_THUMB,
+	L_SHLD,
+	R_SHLD,
+	BUT_A,
+	BUT_B,
+	BUT_X,
+	BUT_Y, 
+	BUTTON_COUNT
 };
 
 static const char* s_button_names[]
 {
-    "DPAD UP", 
-    "DPAD DOWN",
-    "DPAD LEFT",
-    "DPAD RIGHT",
-    "START",
-    "BACK",
-    "LEFT THUMB",
-    "RIGHT THUMB",
-    "LEFT SHLD",
-    "RIGHT SHLD",
-    "A",
-    "B",
-    "X",
-    "Y"   
+	"DPAD UP", 
+	"DPAD DOWN",
+	"DPAD LEFT",
+	"DPAD RIGHT",
+	"START",
+	"BACK",
+	"LEFT THUMB",
+	"RIGHT THUMB",
+	"LEFT SHLD",
+	"RIGHT SHLD",
+	"A",
+	"B",
+	"X",
+	"Y"   
 };
 
 struct Button_State
 {
-    Button_State(bool curr, bool prev) : m_curr(curr), m_prev(prev) {};
-    Button_State() : m_curr(0), m_prev(0) {};
-
-    inline bool is_down() { return m_curr; }
-    inline bool is_up() { return !m_curr; }
-    inline bool is_pressed() { return !m_prev && m_curr; }
-    inline bool is_released() { return m_prev && !m_curr; }
-
-    bool m_curr = 0;
-    bool m_prev = 0;
+	Button_State(bool curr, bool prev) : m_curr(curr), m_prev(prev) {};
+	Button_State() : m_curr(0), m_prev(0) {};
+	
+	inline bool is_down() { return m_curr; }
+	inline bool is_up() { return !m_curr; }
+	inline bool is_pressed() { return !m_prev && m_curr; }
+	inline bool is_released() { return m_prev && !m_curr; }
+	
+	bool m_curr = 0;
+	bool m_prev = 0;
 };
 
 struct Controller_State
 {
-    struct Data
-    {
+	struct Data
+	{
 		u16 button_states = 0;
 		f32 l_thumb_x = 0;
 		f32 l_thumb_y = 0;
@@ -252,38 +252,38 @@ struct Controller_State
 		f32 r_thumb_y = 0;
 		f32 l_trig = 0;
 		f32 r_trig = 0;
-    };
-
-    Controller_State::Data m_curr;
-    Controller_State::Data m_prev;
-
-    Button_State get_button_state(Button button)
-    {
+	};
+	
+	Controller_State::Data m_curr;
+	Controller_State::Data m_prev;
+	
+	Button_State get_button_state(Button button)
+	{
 		i16 b = (i16)button;
 		return Button_State((m_curr.button_states & (1 << (int)b)) > 0, (m_prev.button_states & (1 << (int)b)) > 0);
-    }
-
-    bool get_button_down(Button button)
-    {
+	}
+	
+	bool get_button_down(Button button)
+	{
 		return (m_curr.button_states & (1 << (int)button)) > 0;
-    }
-
-    static constexpr f32 s_default_dead_zone = 0.25f;
-
-    bool get_left_stick_x(f32& output, f32 dead_zone = s_default_dead_zone)
-    {
+	}
+	
+	static constexpr f32 s_default_dead_zone = 0.25f;
+	
+	bool get_left_stick_x(f32& output, f32 dead_zone = s_default_dead_zone)
+	{
 		if (abs(m_curr.l_thumb_x) < dead_zone) 
 			return false;
 		if (m_curr.l_thumb_x > 0)
 			output = (m_curr.l_thumb_x - dead_zone) / (1.f - dead_zone);
 		else
 			output = (m_curr.l_thumb_x + dead_zone) / (-1.f + dead_zone) * -1;
-			
+		
 		return true;
-    }
-
-    bool get_left_stick_y(f32& output, f32 dead_zone = s_default_dead_zone)
-    {
+	}
+	
+	bool get_left_stick_y(f32& output, f32 dead_zone = s_default_dead_zone)
+	{
 		if (abs(m_curr.l_thumb_y) < dead_zone) 
 			return false;
 		if (m_curr.l_thumb_y > 0)
@@ -291,22 +291,22 @@ struct Controller_State
 		else
 			output = (m_curr.l_thumb_y + dead_zone) / (-1.f + dead_zone) * -1;
 		return true;
-    }
-
-    bool get_right_stick_x(f32& output, f32 dead_zone = s_default_dead_zone)
-    {
+	}
+	
+	bool get_right_stick_x(f32& output, f32 dead_zone = s_default_dead_zone)
+	{
 		if (abs(m_curr.r_thumb_x) < dead_zone) 
 			return false;
 		if (m_curr.r_thumb_x > 0)
 			output = (m_curr.r_thumb_x - dead_zone) / (1.f - dead_zone);
 		else
 			output = (m_curr.r_thumb_x + dead_zone) / (-1.f + dead_zone) * -1;
-
+		
 		return true;
-    }
-
-    bool get_right_stick_y(f32& output, f32 dead_zone = s_default_dead_zone)
-    {
+	}
+	
+	bool get_right_stick_y(f32& output, f32 dead_zone = s_default_dead_zone)
+	{
 		if (abs(m_curr.r_thumb_y) < dead_zone) 
 			return false;
 		
@@ -314,8 +314,8 @@ struct Controller_State
 			output = (m_curr.r_thumb_y - dead_zone) / (1.f - dead_zone);
 		else
 			output = (m_curr.r_thumb_y + dead_zone) / (-1.f + dead_zone) * -1;
-
+		
 		return true;
-    }
+	}
 };
 
